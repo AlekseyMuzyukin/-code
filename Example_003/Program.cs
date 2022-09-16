6dc47d6794db82519bd64a41ed245ddd888e1504
Aleksey <J2k_pot@mail.ru>
2022-09-16 11:10:49 +0300

    task_3

---
+++ Example_003/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Введите номер дня недели");
+string numDay = Console.ReadLine();
+if (numDay == "1")
+{
+    Console.WriteLine ("Понедельник");
+}
+else if (numDay == "2")
+{
+    Console.WriteLine ("Вторник");
+} 
+else if (numDay == "3")
+{
+    Console.WriteLine ("Среда");
+} 
+else if (numDay == "4")
+{
+    Console.WriteLine ("Четверг");
+} 
+else if (numDay == "5")
+{
+    Console.WriteLine ("Пятница");
+} 
+else if (numDay == "6")
+{
+    Console.WriteLine ("Суббота");
+} 
+else if (numDay == "7")
+{
+    Console.WriteLine ("Воскресение");
+} 
+else
+{
+    Console.WriteLine ("Вы ввели неверное значение");
+} 
