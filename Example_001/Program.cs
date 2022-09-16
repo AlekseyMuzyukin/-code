261c9e248802dd571de8769ff80d3cc236ea4aa4
Aleksey <J2k_pot@mail.ru>
2022-09-16 10:53:45 +0300

    task_1_1

--- Example_001/Program.cs
+++ Example_001/Program.cs
@@ -7,7 +7,21 @@
 // -3 -> 9
 // -7 -> 49
 
-Console.Write(" Ввведите целое число: ");
-int number = Convert.ToInt32(Console.ReadLine ());
-int square = number * number;
-Console.WriteLine ($"Квадрат числа{number}={square}");
+//Console.Write(" Ввведите целое число: ");
+//int number = Convert.ToInt32(Console.ReadLine ());
+//int square = number * number;
+//Console.WriteLine ($"Квадрат числа{number}={square}");
+
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+int sqr = b * b;
+if (sqr == a)
+{
+    Console.Write($"да ,первое число {a} является квадратом второго числа {b}");
+}
+else 
+{
+    Console.Write($"нет, первое число {a} не является квадратом второго числа {b}");
+}
