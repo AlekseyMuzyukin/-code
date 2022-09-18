975684b125641f2b5d7bde7a3766aece20801073
Aleksey <J2k_pot@mail.ru>
2022-09-18 08:43:45 +0300

    new task_4

--- Example_004/Program.cs
+++ Example_004/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿Console.WriteLine("Введите три числа ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (a > b & a > c) max = a;
+if (b > a & b > c) max = b;
+if (c > a & c > b) max = c;
+Console.WriteLine($"Максимальное число {max}");
